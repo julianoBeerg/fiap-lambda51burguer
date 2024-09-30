186ca64da85847165eeae4af1c2badbb0eea74a8
jnunes <jnunes@magnasistemas.com.br>
2024-09-30 14:36:48 -0300

    feature/deploy: implementando url em secret

--- Lambda51Burguer/src/main/java/fiap/repository/UserRepository.java
+++ Lambda51Burguer/src/main/java/fiap/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package fiap.repository;
 
 import fiap.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 public class UserRepository {
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String BASE_URL = "http://ad14508baa62b43a49dcf64ed225de08-1154354306.us-east-1.elb.amazonaws.com/client";
-
+//    private final String BASE_URL = "http://ad14508baa62b43a49dcf64ed225de08-1154354306.us-east-1.elb.amazonaws.com/client";
+    @Value("${BASE_URL}")
+    private String BASE_URL;
     public User findByCpf(String cpf) {
         try {
             return restTemplate.getForObject(BASE_URL + "/cpf?cpf=" + cpf, User.class);
